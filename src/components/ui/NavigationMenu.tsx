4eaa140e68cb089488c79bdbfb6a1917d2a0ae42
season shrestha <np03a170120@heraldcollege.edu.np>
2024-08-30 14:39:01 +0545

    feat:deployment & updates

--- src/components/ui/NavigationMenu.tsx
+++ src/components/ui/NavigationMenu.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import { Link } from "@tanstack/react-router";
-import Logo from "../../assets/logo.png";
 import {
   AppstoreOutlined,
   DollarOutlined,
   MenuOutlined,
 } from "@ant-design/icons";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { Button } from "antd";
+import { useState } from "react";
+import Logo from "../../assets/logo.png";
 
 const NavigationMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -19,17 +20,22 @@ const NavigationMenu = () => {
     <>
       <Button
         onClick={toggleMenu}
-        className="fixed top-4 left-4 z-50 p-2 bg-primaryHover text-white rounded-md md:hidden"
+        className="fixed top-4 left-4 z-50 p-2 bg-primaryHover text-white rounded-md lg:hidden"
       >
         <MenuOutlined />
       </Button>
 
       <div
-        className={`fixed top-0 left-0 w-64 min-h-screen bg-white px-5 py-2 transition-transform transform ${
+        className={`fixed top-0 left-0 w-64 min-h-screen bg-white px-5 py-2 !pr-0 transition-transform transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
-        } z-40 md:translate-x-0 md:relative md:w-64 md:flex md:flex-col md:bg-white md:px-5 md:py-2`}
+        } z-40 lg:translate-x-0 lg:relative lg:w-64 lg:flex lg:flex-col lg:bg-white lg:px-5 lg:py-2 `}
       >
-        <img className="h-12 w-12 mb-6" src={Logo} alt="Logo" />
+        <img
+          onClick={() => navigate({ to: "/login" })}
+          className="h-12 w-12 mb-6 cursor-pointer"
+          src={Logo}
+          alt="Logo"
+        />
         <ul className="h-full flex flex-col gap-1">
           <li>
             <Link
